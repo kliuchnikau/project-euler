6a4829d8b5a077ea05b23b51b3db0d6a908616fc
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2015-01-04 20:38:08 +0300

    Fix typo

--- 1/solution.hs
+++ 1/solution.hs
@@ -1,6 +1,6 @@
 -- https://projecteuler.net/problem=1
 multipleOf3or5 n = (n `mod` 3 == 0) || (n `mod` 5 == 0)
-sumOfAllMultipes n = sum (filter multipleOf3or5 [1..n-1])
+sumOfAllMultiples n = sum (filter multipleOf3or5 [1..n-1])
 
--- sumOfAllMultipes 1000
+-- sumOfAllMultiples 1000
 -- 233168
