8381d4c06973d4ccb1a0ee5985562bc451b92060
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2015-01-11 23:54:51 +0300

    Define funcion using tuple pattern matching

--- 019/solution.hs
+++ 019/solution.hs
@@ -3,8 +3,10 @@
 allDays :: ((Int,Int,Int),(Int,Int,Int)) -> [(String, Int)]
 
 isMondayOnFirst :: (String, Int) -> Bool
+isMondayOnFirst ("Monday",1) = True
+isMondayOnFirst _ = False
 
 countSundaysOnFirst :: ((Int,Int,Int),(Int,Int,Int)) -> Int
 countSundaysOnFirst (from,to) = length (filter isMondayOnFirst allDays(from, to))
 
-countSundaysOnFirst ((1,1,1991),(31,12,200))
+-- countSundaysOnFirst ((1,1,1991),(31,12,200))
