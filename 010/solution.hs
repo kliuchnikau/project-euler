e6c1cfa735da82430158013a00bb4a67f18b40ef
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2015-01-13 17:45:28 +0300

    Brute force approach with lazy primes sequence and only-prime factors is much more effective

--- 010/solution.hs
+++ 010/solution.hs
@@ -7,3 +7,16 @@ sumPrimesBelow n = sum [ x | x <- [1..n-1], prime x]
 -- sumPrimesBelow 2000000
 -- 142913828922
 -- (519.57 secs, 148427107648 bytes)
+
+otherSumPrimesBelow n = sum [ x | x <- [1..n-1], isPrime x]
+
+primes :: [Integer]
+primes = 2 : [ x | x <- [3..], isPrime x]
+
+isPrime :: Integer -> Bool
+isPrime x = all (\y -> x `mod` y > 0) (candidates x)
+                where
+                  candidates z = takeWhile (\p -> p*p <= z) primes
+
+-- 142913828923
+-- (26.91 secs, 7311696232 bytes)
