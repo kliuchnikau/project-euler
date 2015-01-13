3c5f14e1be4bb9b94b7905aa27815fb4ab74d574
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2015-01-13 17:09:46 +0300

    Optimise prime calculation - ignore even factors, make less checks, do not sort and dedup result

--- lib/Primes.hs
+++ lib/Primes.hs
@@ -1,6 +1,7 @@
-module Primes (factors, factorsUpto, prime) where
+module Primes (factors, prime) where
 import Data.List
 
+--all factors calculation
 factors :: Int -> [Int]
 factors n = factors' n 1 []
 
@@ -14,20 +15,23 @@ factors' num currentFactor result | passedNum || factorAlreadyInList = sort (nub
                                     foundNewFactor = num `mod` currentFactor == 0
                                     currentFactorMultiplier = num `div` currentFactor
 
-factorsUpto :: Int -> Int -> [Int]
-factorsUpto n upperBound = factorsUpto' n 1 [] upperBound
-
-factorsUpto' :: Int -> Int -> [Int] -> Int -> [Int]
-factorsUpto' num currentFactor result upperBound | reachedUpperBound || factorAlreadyInList = sort (nub result)
-                                                 | foundNewFactor = factorsUpto' num (currentFactor+1) (currentFactor:result) upperBound
-                                                 | otherwise = factorsUpto' num (currentFactor+1) result upperBound
-                                                 where
-                                                   reachedUpperBound = currentFactor > upperBound
-                                                   factorAlreadyInList = elem currentFactor result
-                                                   foundNewFactor = num `mod` currentFactor == 0
-
+--prime calculation
 prime :: Int -> Bool
 prime 1 = False
-prime n = (factorsUpto n upperBound) == [1]
+prime 2 = True
+prime n = (potentialPrimeFactorsUpto n upperBound) == [1]
   where
     upperBound = floor (sqrt (fromIntegral n))
+
+potentialPrimeFactorsUpto :: Int -> Int -> [Int]
+potentialPrimeFactorsUpto n upperBound = potentialPrimeFactorsUpto' n 3 startingResult upperBound
+  where startingResult = if even n then [1,2] else [1]
+
+potentialPrimeFactorsUpto' :: Int -> Int -> [Int] -> Int -> [Int]
+potentialPrimeFactorsUpto' num currentFactor result upperBound | reachedUpperBound = result
+                                                               | foundNewFactor = potentialPrimeFactorsUpto' num (currentFactor+2) (currentFactor:result) upperBound
+                                                               | otherwise = potentialPrimeFactorsUpto' num (currentFactor+2) result upperBound
+                                                               where
+                                                                 reachedUpperBound = currentFactor > upperBound
+                                                                 foundNewFactor = num `mod` currentFactor == 0
+
