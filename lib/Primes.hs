4ed0e3c32eb1885651a86e21bb890b5b42462931
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2015-01-13 16:21:10 +0300

    Optimised prime function - get factors up to sqrt of a number

--- lib/Primes.hs
+++ lib/Primes.hs
@@ -1,12 +1,11 @@
-module Primes (factors, prime) where
+module Primes (factors, factorsUpto, prime) where
 import Data.List
 
 factors :: Int -> [Int]
 factors n = factors' n 1 []
 
 factors' :: Int -> Int -> [Int] -> [Int]
-factors' num currentFactor result | passedNum = sort (nub result)
-                                  | factorAlreadyInList = sort (nub result)
+factors' num currentFactor result | passedNum || factorAlreadyInList = sort (nub result)
                                   | foundNewFactor = factors' num (currentFactor+1) (currentFactor:currentFactorMultiplier:result)
                                   | otherwise = factors' num (currentFactor+1) result
                                   where
@@ -15,5 +14,19 @@ factors' num currentFactor result | passedNum = sort (nub result)
                                     foundNewFactor = num `mod` currentFactor == 0
                                     currentFactorMultiplier = num `div` currentFactor
 
+factorsUpto :: Int -> Int -> [Int]
+factorsUpto n upperBound = factorsUpto' n 1 [] upperBound
+
+factorsUpto' :: Int -> Int -> [Int] -> Int -> [Int]
+factorsUpto' num currentFactor result upperBound | reachedUpperBound || factorAlreadyInList = sort (nub result)
+                                                 | foundNewFactor = factorsUpto' num (currentFactor+1) (currentFactor:result) upperBound
+                                                 | otherwise = factorsUpto' num (currentFactor+1) result upperBound
+                                                 where
+                                                   reachedUpperBound = currentFactor > upperBound
+                                                   factorAlreadyInList = elem currentFactor result
+                                                   foundNewFactor = num `mod` currentFactor == 0
+
 prime :: Int -> Bool
-prime x = (factors x) == [1,x]
+prime n = (factorsUpto n upperBound) == [1]
+  where
+    upperBound = floor (sqrt (fromIntegral n))
