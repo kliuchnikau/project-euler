5ac5fce282be85fbbb46e90d205f126eb179a373
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2015-01-14 17:39:08 +0300

    Remove unneeded methods calls - replace them with simple length

--- 025/solution.hs
+++ 025/solution.hs
@@ -2,8 +2,8 @@
 import Fibonacci
 import Digits
 
-firstNDigitsFibonacciNumTermNo :: Int -> Integer
-firstNDigitsFibonacciNumTermNo n = 1 + (snd $ head $ reverse $ zip (takeWhile (\fval -> digitsCount(fval) < n) fibonaccies) [2..])
+firstNDigitsFibonacciNumTermNo :: Int -> Int
+firstNDigitsFibonacciNumTermNo n = 2 + (length $ takeWhile (\fval -> digitsCount(fval) < n) fibonaccies)
 
 -- firstNDigitsFibonacciNumTermNo 1000
 -- (31.29 secs, 46395558216 bytes)
