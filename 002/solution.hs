57947afa8045b1fde2eeb3e26422895608bf2a7b
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2015-01-14 17:47:27 +0300

    Problem 2 does not need to import Digits

--- 002/solution.hs
+++ 002/solution.hs
@@ -1,6 +1,5 @@
 -- https://projecteuler.net/problem=2
 import Fibonacci
-import Digits
 
 sumEvenFibonacciNumbers :: Integer -> Integer
 sumEvenFibonacciNumbers max = sum (filter even (takeWhile (\fval -> fval <= max) fibonaccies))
