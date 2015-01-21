8c2714d69873c68258acf28b179f7a32f59b45c6
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2015-01-21 20:49:56 +0300

    First implementation of calendar problem

--- 019/solution.hs
+++ 019/solution.hs
@@ -1,12 +1,45 @@
 -- https://projecteuler.net/problem=19
 
-allDays :: ((Int,Int,Int),(Int,Int,Int)) -> [(String, Int)]
+numOfDaysInMonth (dayNo,monthNo,yearNo)
+  | (monthNo == 2) && isLeapYear = 29
+  | (monthNo == 2) && not isLeapYear = 28
+  | elem monthNo [4,6,9,11] = 30
+  | otherwise = 31
+  where
+    isLeapYear = yearDivisibleBy4 && (notCenturyYear || centuryDivisibleBy400)
+    yearDivisibleBy4 = yearNo `mod` 4 == 0
+    notCenturyYear = not (yearNo `mod` 100 == 0)
+    centuryDivisibleBy400 = yearNo `mod` 400 == 0
 
-isMondayOnFirst :: (String, Int) -> Bool
-isMondayOnFirst ("Monday",1) = True
+nextDayOfWeek :: String -> String
+nextDayOfWeek "Unknown" = "Unknown"
+nextDayOfWeek currentDayOfWeek = daysOfWeek !! ((currentDayIdx+1) `mod` 7)
+  where
+    currentDayIdx = snd $ head $ filter (\(day,idx) -> day == currentDayOfWeek) (daysOfWeek `zip` [0..6])
+    daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Trursday", "Friday", "Saturday", "Sunday"]
+
+nextDay :: ((Int,Int,Int), String) -> ((Int,Int,Int), String)
+nextDay ((dayNo,monthNo,yearNo), currentDayOfWeek) = (nextDate daysInMonth, nextDayOfWeek currentDayOfWeek)
+  where
+    daysInMonth = numOfDaysInMonth (dayNo,monthNo,yearNo)
+    nextDate daysInMonth | dayNo == daysInMonth = if monthNo == 12 then (1,1,yearNo+1) else (1,monthNo+1,yearNo)
+                         | otherwise = (dayNo+1,monthNo,yearNo)
+
+allDays :: [((Int,Int,Int), String)]
+allDays = ((1,1,1900), "Monday") : [nextDay(day num) | num <- [0..]]
+  where
+    day dayNum = allDays !! dayNum
+
+daysRange :: ((Int,Int,Int),(Int,Int,Int)) -> [((Int,Int,Int), String)]
+daysRange (startDay,endDay) = takeWhile (\(day,dayOfWeek) -> not $ day == dayAfterEnd) $ dropWhile (\(day,dayOfWeek) -> not $ day == startDay) allDays
+  where
+    dayAfterEnd = fst $ nextDay(endDay, "Unknown")
+
+isMondayOnFirst :: ((Int,Int,Int), String) -> Bool
+isMondayOnFirst ((1,_,_), "Monday") = True
 isMondayOnFirst _ = False
 
-countSundaysOnFirst :: ((Int,Int,Int),(Int,Int,Int)) -> Int
-countSundaysOnFirst (from,to) = length (filter isMondayOnFirst allDays(from, to))
+countMondaysOnFirst :: ((Int,Int,Int),(Int,Int,Int)) -> Int
+countMondaysOnFirst (from,to) = length [ day | day <- daysRange(from, to), isMondayOnFirst day]
 
--- countSundaysOnFirst ((1,1,1991),(31,12,200))
+-- countMondaysOnFirst ((1,1,1991),(31,12,2000))
