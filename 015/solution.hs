611b4c416c1de00944253f696a8c32c0e077478a
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2015-01-24 11:26:10 +0300

    Optimised graph traversing by memoizing known paths in the matrix

--- 015/solution.hs
+++ 015/solution.hs
@@ -1,10 +1,14 @@
 -- https://projecteuler.net/problem=15
+import qualified Data.Matrix as Matrix
 
 countPaths :: (Int,Int) -> (Int,Int) -> (Int,Int) -> Int
-countPaths (maxX, maxY) (fromX,fromY) (toX,toY)
-  | (fromX > maxX) || (fromY > maxY) = 0
-  | (fromX == toX) && (fromY == toY) = 1
-  | otherwise = countPaths (maxX, maxY) (fromX+1,fromY) (toX, toY) + countPaths (maxX, maxY) (fromX,fromY+1) (toX, toY)
+countPaths (maxX, maxY) (toX,toY) = takePathsCount
+  where
+   takePathsCount (fromX,fromY) = pathCountsCache Matrix.! (fromX,fromY)
+   pathCountsCache = Matrix.fromLists [[ countPaths'(x,y) | x <- [1..maxX+1]] | y <- [1..maxY+1]]
+   countPaths' (fromX,fromY)
+     | (fromX > maxX) || (fromY > maxY) = 0
+     | (fromX == toX) && (fromY == toY) = 1
+     | otherwise = takePathsCount (fromX+1,fromY) + takePathsCount (fromX,fromY+1)
 
---countPaths (2,2) (0,0) (2,2)
---countPaths (20, 20) (0,0) (20,20)
+--countPaths (21, 21) (21,21) (1,1)
