b8fa4d3f59758bcc6ea8111e02fa2cb0fa43d36e
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2015-01-26 21:44:03 +0300

    First iteration of recursive building of a product matrix bottom-up

--- 018/solution.hs
+++ 018/solution.hs
@@ -20,7 +20,20 @@ buildMatrix input = Matrix.fromLists $ adjustListsToSameLength $ map listOfInts 
     listOfInts str = map (\intStr -> read intStr :: Integer) $ splitOn " " str
 
 buildProductMatrix :: Matrix.Matrix Integer -> Matrix.Matrix Integer
-buildProductMatrix = undefined
+buildProductMatrix matrix = buildProductMatrixRow (Matrix.nrows matrix - 1) matrix
+  where
+    buildProductMatrixRow :: Int -> Matrix.Matrix Integer -> Matrix.Matrix Integer
+    buildProductMatrixRow forRow matrix
+      | forRow == 0 = matrix
+      | otherwise = buildProductMatrixRow (forRow-1) $ buildProductMatrixCells (1, forRow) matrix
+    buildProductMatrixCells :: (Int,Int) -> Matrix.Matrix Integer -> Matrix.Matrix Integer
+    buildProductMatrixCells (x,y) matrix
+      | x > y = matrix
+      | otherwise = buildProductMatrixCells (x+1,y) $ Matrix.setElem (cellProductValue (x,y) matrix) (x,y) matrix
+    cellProductValue (x,y) matrix = max (val (x,y) * val (x,y+1)) (val (x,y) * val (x+1,y+1))
+      where
+        val (x,y) = matrix Matrix.! (x,y)
+
 
 getMaxProduct :: String -> IO Integer
 getMaxProduct fileName = do
