af2073fa9648c6f91bcaf81c488524b15f05d8b4
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2015-02-26 14:25:43 +0300

    For construction of a unique list use set which is O(n*logn) instead of nub which is O(n^2) and too slow for the task

--- 023/solution.hs
+++ 023/solution.hs
@@ -1,5 +1,7 @@
 import Factors
 import Data.List
+import qualified Data.Set as Set
+
 
 sumDivisors :: Int -> Int
 sumDivisors = sum . deleteLast . factors
@@ -13,15 +15,21 @@ abundantNumsUnder n = map fst $ filter isAbundant (sumDivisorsUnder n)
   where
     isAbundant (num,sumDivs) = sumDivs > num
 
+knownMinNeverSumOfAbundants = 28123
+
+allAbundants :: [Int]
+allAbundants = abundantNumsUnder knownMinNeverSumOfAbundants
+
+allSumsOf2Abundants :: [Int]
+allSumsOf2Abundants = Set.toList $ Set.fromList [x + y | x <- allAbundants, y <- allAbundants, x + y <= knownMinNeverSumOfAbundants]
+
 findNotSumOfAbundants :: Int
 findNotSumOfAbundants = sum notSumOfAbundants
   where
-    knownMinNeverSumOfAbundants = 28123
     allPossibleNumbers = [1..knownMinNeverSumOfAbundants]
-    allAbundants = abundantNumsUnder knownMinNeverSumOfAbundants
-    allSumsOf2Abundants = undefined
     notSumOfAbundants = allPossibleNumbers \\ allSumsOf2Abundants
 
 -- works 110 sec in ghci. 8 sec in ghc.
-main = do
-  print findNotSumOfAbundants
+--main = print findNotSumOfAbundants
+--main = print $ length allAbundants : 6965, 11sec
+main = print $ length allSumsOf2Abundants -- 27sec
