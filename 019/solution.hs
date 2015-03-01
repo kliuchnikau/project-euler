e9fa6808f9ca47f4795d4afed38a8cf158310696
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2015-03-01 18:51:20 +0300

    Simplify tautology in problem 19 solution by declaring Date type

--- 019/solution.hs
+++ 019/solution.hs
@@ -1,4 +1,5 @@
 -- https://projecteuler.net/problem=19
+type Date = (Int,Int,Int)
 
 numOfDaysInMonth (dayNo,monthNo,yearNo)
   | (monthNo == 2) && isLeapYear = 29
@@ -18,28 +19,28 @@ nextDayOfWeek currentDayOfWeek = daysOfWeek !! ((currentDayIdx+1) `mod` 7)
     currentDayIdx = snd $ head $ filter (\(day,idx) -> day == currentDayOfWeek) (daysOfWeek `zip` [0..6])
     daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-nextDay :: ((Int,Int,Int), String) -> ((Int,Int,Int), String)
+nextDay :: (Date, String) -> (Date, String)
 nextDay ((dayNo,monthNo,yearNo), currentDayOfWeek) = (nextDate daysInMonth, nextDayOfWeek currentDayOfWeek)
   where
     daysInMonth = numOfDaysInMonth (dayNo,monthNo,yearNo)
     nextDate daysInMonth | dayNo == daysInMonth = if monthNo == 12 then (1,1,yearNo+1) else (1,monthNo+1,yearNo)
                          | otherwise = (dayNo+1,monthNo,yearNo)
 
-allDays :: [((Int,Int,Int), String)]
+allDays :: [(Date, String)]
 allDays = ((1,1,1900), "Monday") : [nextDay(day num) | num <- [0..]]
   where
     day dayNum = allDays !! dayNum
 
-daysRange :: ((Int,Int,Int),(Int,Int,Int)) -> [((Int,Int,Int), String)]
+daysRange :: (Date,Date) -> [(Date, String)]
 daysRange (startDay,endDay) = takeWhile (\(day,dayOfWeek) -> not $ day == dayAfterEnd) $ dropWhile (\(day,dayOfWeek) -> not $ day == startDay) allDays
   where
     dayAfterEnd = fst $ nextDay(endDay, "Unknown")
 
-isSundayOnFirst :: ((Int,Int,Int), String) -> Bool
+isSundayOnFirst :: (Date, String) -> Bool
 isSundayOnFirst ((1,_,_), "Sunday") = True
 isSundayOnFirst _ = False
 
-countSundaysOnFirst :: ((Int,Int,Int),(Int,Int,Int)) -> Int
+countSundaysOnFirst :: (Date,Date) -> Int
 countSundaysOnFirst (from,to) = length [ day | day <- daysRange(from, to), isSundayOnFirst day]
 
 -- countSundaysOnFirst ((1,1,1901),(31,12,2000))
