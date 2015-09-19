54e0624958cdef5cd88c049737f2a91d1c746cfb
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2015-09-19 17:29:19 +0200

    Simple solution for quadratic prime generator

--- 027/Solution.hs
+++ 027/Solution.hs
@@ -2,18 +2,25 @@ module Solution where
 
 import Primes
 import Data.List
+import Data.Function
 
-primesStreakLength :: (Int, Int) -> Int
-primesStreakLength (a,b) = indexToCountConvert $ last $ takeWhile (isPrime . quadraticFormula a b) consecutiveValuesOfN
+type CoefficentsPair = (Int, Int)
+
+primesStreakLength :: CoefficentsPair -> Int
+primesStreakLength (a,b) = indexToCountConvert $ last $ -1 : takeWhile producePrimeValue consecutiveValuesOfN
   where
-    indexToCountConvert = (1 +)
-    quadraticFormula a b n = n^2 + a*n + b
     consecutiveValuesOfN = [0..]
+    producePrimeValue = isPrime . quadraticFormula a b
+    quadraticFormula a b n = n^2 + a*n + b
+    indexToCountConvert = (1 +)
 
-{-coefficientsWithLongestStreak :: (Int, Int)-}
-{-coefficientsWithLongestStreak = maximumBy primesStreakLength [ (a,b) | a <- [-limit..limit], b <- [-limit..limit] ]-}
-  {-where-}
-    {-limit = 10  1000-}
+allPossibleCoefficients :: [CoefficentsPair]
+allPossibleCoefficients = [ (a,b) | a <- [-limit..limit], b <- [-limit..limit] ]
+  where
+    limit = 100
+
+coefficientsWithLongestStreak :: CoefficentsPair
+coefficientsWithLongestStreak = maximumBy (compare `on` primesStreakLength) allPossibleCoefficients
 
 {-findProductOfCoefficients :: Int-}
 {-findProductOfCoefficients = productPair coefficientsWithLongestStreak-}
--- 027/SolutionSpec.hs
+++ 027/SolutionSpec.hs
@@ -6,8 +6,12 @@ import Solution
 main :: IO ()
 main = hspec $ do
   describe "primesStreakLength" $ do
-    specify "known example 1 (Euler formula)" $
+    specify "known example 1 (Euler formula)" $ do
       primesStreakLength (1,41) `shouldBe` 40
 
-    specify "known example 2" $
+    specify "known example 2" $ do
       primesStreakLength (-79,1601) `shouldBe` 80
+
+    context "when produces not prime number straight away" $ do
+      it "returns 0" $ do
+        primesStreakLength (-10,4) `shouldBe` 0
