8e4bafb54cc60931eea4ab2443dacc7aea9cc0a3
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2015-10-04 14:24:48 +0200

    Add function that finds all natural roots of quadratic equation

--- 045/QuadraticEquation.hs
+++ 045/QuadraticEquation.hs
@@ -1,10 +1,12 @@
 module QuadraticEquation where
 
+import Data.List
+
 data QEquation = QEquation { aVal :: Double, bVal :: Double, cVal :: Double } deriving (Show)
 
--- find roots of a*x^2 + bx + c = 0
-solveEquation :: QEquation -> (Double, Double)
-solveEquation (QEquation {aVal=a, bVal=b, cVal=c}) = if discriminant<0 then error "negative discriminant" else (x,y)
+-- when discriminant < 0 we get complex numbers. for now let's ignore complex roots
+equationRoots :: QEquation -> [Double]
+equationRoots (QEquation {aVal=a, bVal=b, cVal=c}) = if discriminant<0 then [] else nub [x,y]
   where
     discriminant = b^2 - 4*a*c
     x = (-b + sqrt discriminant) / (2*a)
@@ -12,3 +14,13 @@ solveEquation (QEquation {aVal=a, bVal=b, cVal=c}) = if discriminant<0 then erro
 
 substituteUnknown :: QEquation -> Double -> Double
 substituteUnknown (QEquation {aVal=a, bVal=b, cVal=c}) x = a*x^2 + b*x + c
+
+naturalRoots :: QEquation -> [Integer]
+naturalRoots eq = filter stillEquationRoot $ toNaturalNumbers $ equationRoots eq
+  where
+    toNaturalNumbers :: [Double] -> [Integer]
+    toNaturalNumbers xs = filter (>= 0) $ map (toInteger . round) xs
+    stillEquationRoot :: Integer -> Bool
+    stillEquationRoot x = (substituteUnknown eq value) == 0.0
+      where
+        value = fromIntegral x :: Double
--- 045/QuadraticEquationSpec.hs
+++ 045/QuadraticEquationSpec.hs
@@ -5,11 +5,15 @@ import QuadraticEquation
 
 main :: IO ()
 main = hspec $ do
-  describe "solveEquation" $ do
+  describe "equationRoots" $ do
     context "when equation has non-complex solution" $ do
       it "returns two roots of equation" $ do
-        solveEquation (QEquation 1.0 5.0 6.0) `shouldBe` (-2.0, -3.0)
+        equationRoots (QEquation 1.0 5.0 6.0) `shouldBe` [-2.0, -3.0]
 
   describe "substituteUnknown" $ do
     it "substitutes x with the specified value" $ do
       substituteUnknown (QEquation 1.0 5.0 6.0) (-3.0) `shouldBe` 0.0
+
+  describe "naturalRoots" $ do
+    it "returns roots only if they are natural numbers" $ do
+      naturalRoots (QEquation 0.5 0.5 (-40755.0)) `shouldBe` [285]
